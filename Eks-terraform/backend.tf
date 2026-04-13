978178cda1373c3ef7e750550b7677e589d32aa9
Sohebpathan <shohebpathan12344@gmail.com>
2026-04-13 14:18:45 +0530

    Update S3 bucket name in backend

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "Soheb_bucket1" # Replace with your actual S3 bucket name
+    bucket = "chatbotbuck-1234" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-2"
   }
